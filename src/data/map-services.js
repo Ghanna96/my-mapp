e613e52cc76191671b9f06bc68fc30b66ab8f196
Ghanna96 <akramghanna@outlook.com>
2020-08-21 20:06:25 +0200

    map with data!

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,4 +10,11 @@ export const mapServices = [
     url:
       'https://gibs-{s}.earthdata.nasa.gov/wmts/epsg3857/best/BlueMarble_ShadedRelief_Bathymetry/default//EPSG3857_500m/{z}/{y}/{x}.jpeg',
   },
+  {
+    name: 'Mapbox',
+    attribution:
+      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    url:
+      'https://api.mapbox.com/styles/v1/akramghanname/ckdymzmfn57131aorkfdszy0f/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWtyYW1naGFubmFtZSIsImEiOiJja2R5bXZ6bngxZmpkMnlwaWg0bG5maDI3In0.AGaRV7BMtJbyHcpq72y6pw',
+  },
 ];
